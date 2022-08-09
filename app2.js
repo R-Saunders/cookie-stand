9b7d71bee64ffc7b86f2721460d81ba3fafbd741
R-Saunders <richsaunders@protonmail.com>
2022-08-09 19:07:50 +0100

    Add RNG

--- app2.js
+++ app2.js
@@ -9,14 +9,36 @@ let liverpool   = {};
 console.log(typeof(norwich));
 console.log(norwich);
 
-// Locations into array
-const locations = [norwich, london, manchester, birmingham, liverpool];
-
-// Methods created to give values to 1 location
+// Methods created to give values to all location
 liverpool.minCustomers = 16;
 liverpool.maxCustomers = 78;
 liverpool.averageCookiesSoldPerCustomer = 4;
 
-// Checking values saved
-console.log(liverpool);
+norwich.minCustomers = 33;
+norwich.maxCustomers = 56;
+norwich.averageCookiesSoldPerCustomer = 4;
 
+london.minCustomers = 25;
+london.maxCustomers = 45;
+london.averageCookiesSoldPerCustomer = 4;
+
+birmingham.minCustomers = 55;
+birmingham.maxCustomers = 89;
+birmingham.averageCookiesSoldPerCustomer = 4;
+
+manchester.minCustomers = 4;
+manchester.maxCustomers = 12;
+manchester.averageCookiesSoldPerCustomer = 4;
+
+// Locations into array
+const locations = [norwich, london, manchester, birmingham, liverpool];
+
+// Checking it's worked
+console.log(locations);
+
+// Create a random number up to 100.
+liverpool.randomNumber = function() {
+    return Math.floor(Math.random()*100);
+};
+// Checking it worked
+console.log(liverpool.randomNumber());
