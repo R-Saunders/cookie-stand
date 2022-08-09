69b45cef23ce5dd755b8a6317234bfc3d82d05c5
R-Saunders <richsaunders@protonmail.com>
2022-08-09 18:38:04 +0100

    created location objects

---
+++ app2.js
@@ -0,0 +1,11 @@
+let norwich = {};
+let london = {};
+let manchester = {};
+let birmingham = {};
+let liverpool = {};
+
+console.log(typeof(norwich));
+console.log(norwich);
+
+
+const locations = {norwich, london, manchester, birmingham, liverpool};
