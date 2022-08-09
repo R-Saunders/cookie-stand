f4b9ecb31359b8df3c7a1099849c40301ef3e81a
R-Saunders <richsaunders@protonmail.com>
2022-08-09 19:13:18 +0100

    prettify

--- app2.js
+++ app2.js
@@ -10,25 +10,25 @@ console.log(typeof(norwich));
 console.log(norwich);
 
 // Methods created to give values to all location
-liverpool.minCustomers = 16;
-liverpool.maxCustomers = 78;
-liverpool.averageCookiesSoldPerCustomer = 4;
+liverpool.minCustomers                      = 16;
+liverpool.maxCustomers                      = 78;
+liverpool.averageCookiesSoldPerCustomer     = 4;
 
-norwich.minCustomers = 33;
-norwich.maxCustomers = 56;
-norwich.averageCookiesSoldPerCustomer = 4;
+norwich.minCustomers                        = 33;
+norwich.maxCustomers                        = 56;
+norwich.averageCookiesSoldPerCustomer       = 4;
 
-london.minCustomers = 25;
-london.maxCustomers = 45;
-london.averageCookiesSoldPerCustomer = 4;
+london.minCustomers                         = 25;
+london.maxCustomers                         = 45;
+london.averageCookiesSoldPerCustomer        = 4;
 
-birmingham.minCustomers = 55;
-birmingham.maxCustomers = 89;
-birmingham.averageCookiesSoldPerCustomer = 4;
+birmingham.minCustomers                     = 55;
+birmingham.maxCustomers                     = 89;
+birmingham.averageCookiesSoldPerCustomer    = 4;
 
-manchester.minCustomers = 4;
-manchester.maxCustomers = 12;
-manchester.averageCookiesSoldPerCustomer = 4;
+manchester.minCustomers                     = 4;
+manchester.maxCustomers                     = 12;
+manchester.averageCookiesSoldPerCustomer    = 4;
 
 // Locations into array
 const locations = [norwich, london, manchester, birmingham, liverpool];
