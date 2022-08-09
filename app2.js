f07fb0082252f022243b0f96d4ca6458207627f5
R-Saunders <richsaunders@protonmail.com>
2022-08-09 18:48:54 +0100

    Adding comments

--- app2.js
+++ app2.js
@@ -1,17 +1,22 @@
-let norwich = {};
-let london = {};
-let manchester = {};
-let birmingham = {};
-let liverpool = {};
+// Created locations as objects
+let norwich     = {};
+let london      = {};
+let manchester  = {};
+let birmingham  = {};
+let liverpool   = {};
 
+// Checking what's what
 console.log(typeof(norwich));
 console.log(norwich);
 
+// Locations into array
+const locations = [norwich, london, manchester, birmingham, liverpool];
 
-const locations = {norwich, london, manchester, birmingham, liverpool};
-
+// Methods created to give values to 1 location
 liverpool.minCustomers = 16;
 liverpool.maxCustomers = 78;
 liverpool.averageCookiesSoldPerCustomer = 4;
 
+// Checking values saved
 console.log(liverpool);
+
